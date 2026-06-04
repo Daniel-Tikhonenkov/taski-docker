5c0276ff2e3265751da79d658bc31fa44dc4f8b9
Daniel Tikhonenkov <eceoauditore98@gmail.com>
2026-06-04 16:44:37 +0300

    add_deploy_V2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'privetiktaski.sytes.net', '89.169.190.194', 'backend', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'privetiktaski.didns.ru', '89.169.190.194', 'backend', '127.0.0.1']
 
 
 # Application definition
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
